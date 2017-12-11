ba0c3f2ba689bb32abe1bcc4fe7ff0d2f82a50ab
githubjesse <jesseburke25@hotmail.com>
2017-12-11 19:22:34 -0400

    Ready to seed database and prepare the new activities for collecting the information from the seeded data

--- app/src/main/java/com/example/itguest/movietrailerapp/MainActivity.java
+++ app/src/main/java/com/example/itguest/movietrailerapp/MainActivity.java
@@ -4,15 +4,21 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import static com.example.itguest.movietrailerapp.R.id.parent;
+
 public class MainActivity extends AppCompatActivity {
 
     DatabaseHelper trailerDB;
+    Button btnAddTrailer;
+    ListView listViewTrailers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,31 +28,57 @@ public class MainActivity extends AppCompatActivity {
         //Create an instance of the DatabaseHelper Class
         trailerDB = new DatabaseHelper(this);
 
+        /** SEEDING DATABASE */
+
+        trailerDB.addTrailer(new Trailer(1));
+        trailerDB.addTrailer(new Trailer(2));
+        trailerDB.addTrailer(new Trailer(3));
+        trailerDB.addTrailer(new Trailer(4));
+        trailerDB.addTrailer(new Trailer(5));
+
+        /** DONE SEEDING */
+
         //Set up a variable to read from the add Trailer button
-        Button btnAddTrailer = (Button) findViewById(R.id.btnAddTrailer);
+        btnAddTrailer = (Button) findViewById(R.id.btnAddTrailer);
 
         //Set onClickListener to intent to a new page and display a form for adding a new trailer
 //        btnAddTrailer.setOnClickListener({
-              //Create new intent to move to the addTrailerForm
+//            //Create new intent to move to the addTrailerForm
 //            Intent addNewTrailerForm = new Intent(MainActivity.this, addNewTrailerForm.class);
-              //Set flag to start a new task fresha dn destroy the old task
+//            //Set flag to start a new task fresha dn destroy the old task
 //            addNewTrailerForm.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-              //Start new activity and destroy the old one
+//
+//            //Start new activity and destroy the old one
 //            startActivity(addNewTrailerForm);
 //            MainActivity.this.finish();
 //        });
-    }
+
 
     //Retrieve all trailer records from the Database
     ArrayList<Trailer> trailers = trailerDB.getTrailers();
 
     //Set up variable to access the trailers list
-    ListView listViewTrailers = (ListView) findViewById(R.id.listViewTrailers);
+    listViewTrailers = (ListView) findViewById(R.id.listViewTrailers);
 
     //Create adapter object to modify the values in the listView
-//    ListAdapter adapter = new TrailerListAdapter(this, trailers);
-//    listViewTrailers.setAdapter(adapter);
+    ListAdapter adapter = new TrailerListAdapter(this, trailers);
+    listViewTrailers.setOnItemClickListener(new AdapterView.OnItemClickListener()
+    {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+//            Trailer trailer = (Trailer)adapterView.getItemAtPosition(position);
+//            Intent intent = Intent(MainActivity.this, ViewSelectedTrailer.class);
+//            //Set flag to start a new task fresha dn destroy the old task
+//            ViewSelectedTrailer.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 //
-//    listViewTrailers.setOnItemClickListener()
+//            //Start new activity and destroy the old one
+//            startActivity(ViewSelectedTrailer);
+//            MainActivity.this.finish();
+        }
+    });
+
+    //listViewTrailers.setOnItemClickListener()
+
+    }
 }
--- app/src/main/java/com/example/itguest/movietrailerapp/TrailerListAdapter.java
+++ app/src/main/java/com/example/itguest/movietrailerapp/TrailerListAdapter.java
@@ -1,15 +1,55 @@
 package com.example.itguest.movietrailerapp;
 
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextClock;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
-//public class TrailerListAdapter extends ArrayAdapter<Trailer>
-//{
-//    TrailerListAdapter(Context context, ArrayList<Trailer> trailers)
-//    {
-//        super(context, R.layout.trailer_list_item, trailers);
-//    }
-//}
+public class TrailerListAdapter extends ArrayAdapter<Trailer>
+{
+    TrailerListAdapter(Context context, ArrayList<Trailer> trailers)
+    {
+        super(context, R.layout.trailer_list_item, trailers);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        @SuppressLint("ViewHolder") View customView = inflater.inflate(R.layout.trailer_list_item,parent,false);
+
+        //Select a single item from the trailer list
+        Trailer trailerItem = getItem(position);
+
+        //Set up a var to access the trailer list item title textview + video thumbnail
+        TextView textViewVideoTitle = (TextView) customView.findViewById(R.id.textViewTitle);
+        ImageView trailerThumbnail = (ImageView) customView.findViewById(R.id.videoThumbnail);
+
+        //Grab the current item's title and set it's value to the textview
+        textViewVideoTitle.setText(trailerItem.getTitle());
+
+        //Retrieve YouTube Thumbnail via Picasso
+        Picasso.with(getContext())
+                //Load the url path the the thumbnail
+                .load("https://img.youtube.com/vi/"+trailerItem.getVideo_url()+"/0.jpg")
+                //Resize to fit into the list display
+                .resize(250,200)
+                //Set the imageView to set the thumbnail into
+                .into(trailerThumbnail);
+
+        return customView;
+    }
+}
