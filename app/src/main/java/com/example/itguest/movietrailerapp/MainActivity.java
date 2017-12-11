019f3af60a4f9d56629f042dbb44d2520b750ca7
githubjesse <jesseburke25@hotmail.com>
2017-12-11 18:32:40 -0400

    Set up xml layout for the item row in the trailer list

--- app/src/main/java/com/example/itguest/movietrailerapp/MainActivity.java
+++ app/src/main/java/com/example/itguest/movietrailerapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -25,8 +27,12 @@ public class MainActivity extends AppCompatActivity {
 
         //Set onClickListener to intent to a new page and display a form for adding a new trailer
 //        btnAddTrailer.setOnClickListener({
+              //Create new intent to move to the addTrailerForm
 //            Intent addNewTrailerForm = new Intent(MainActivity.this, addNewTrailerForm.class);
+              //Set flag to start a new task fresha dn destroy the old task
 //            addNewTrailerForm.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+              //Start new activity and destroy the old one
 //            startActivity(addNewTrailerForm);
 //            MainActivity.this.finish();
 //        });
@@ -35,5 +41,12 @@ public class MainActivity extends AppCompatActivity {
     //Retrieve all trailer records from the Database
     ArrayList<Trailer> trailers = trailerDB.getTrailers();
 
+    //Set up variable to access the trailers list
+    ListView listViewTrailers = (ListView) findViewById(R.id.listViewTrailers);
 
+    //Create adapter object to modify the values in the listView
+//    ListAdapter adapter = new TrailerListAdapter(this, trailers);
+//    listViewTrailers.setAdapter(adapter);
+//
+//    listViewTrailers.setOnItemClickListener()
 }
---
+++ app/src/main/java/com/example/itguest/movietrailerapp/TrailerListAdapter.java
@@ -0,0 +1,15 @@
+package com.example.itguest.movietrailerapp;
+
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+//public class TrailerListAdapter extends ArrayAdapter<Trailer>
+//{
+//    TrailerListAdapter(Context context, ArrayList<Trailer> trailers)
+//    {
+//        super(context, R.layout.trailer_list_item, trailers);
+//    }
+//}
