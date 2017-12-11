5e00cc73264ce8ae03746723e1f27488b9997a9c
githubjesse <jesseburke25@hotmail.com>
2017-12-11 17:58:38 -0400

    Setting up MainActivity Class.
    Creating Intents to go to the AddTrailerForm and setting DB queries to populate the TrailersListView

--- app/src/main/java/com/example/itguest/movietrailerapp/MainActivity.java
+++ app/src/main/java/com/example/itguest/movietrailerapp/MainActivity.java
@@ -1,17 +1,39 @@
 package com.example.itguest.movietrailerapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseHelper myDB;
+    DatabaseHelper trailerDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myDB = new DatabaseHelper(this);
+        //Create an instance of the DatabaseHelper Class
+        trailerDB = new DatabaseHelper(this);
+
+        //Set up a variable to read from the add Trailer button
+        Button btnAddTrailer = (Button) findViewById(R.id.btnAddTrailer);
+
+        //Set onClickListener to intent to a new page and display a form for adding a new trailer
+//        btnAddTrailer.setOnClickListener({
+//            Intent addNewTrailerForm = new Intent(MainActivity.this, addNewTrailerForm.class);
+//            addNewTrailerForm.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            startActivity(addNewTrailerForm);
+//            MainActivity.this.finish();
+//        });
     }
+
+    //Retrieve all trailer records from the Database
+    ArrayList<Trailer> trailers = trailerDB.getTrailers();
+
+
 }
